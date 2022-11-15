2248005e00150db8e6b868c67f974dfa286f430a
BogdanShvetss <bohdanshvets11@gmail.com>
2022-11-15 21:40:23 +0200

    fix

--- src/test/java/api/UserApiTest.java
+++ src/test/java/api/UserApiTest.java
@@ -54,7 +54,7 @@ public class UserApiTest {
                 .extract()
                 .statusCode();
 
-        Assert.assertEquals(statusCode, 201,
+        Assert.assertEquals(statusCode, 200,
                 String.format("Unexpected status code in response after log in - (%s)", statusCode));
     }
 }
